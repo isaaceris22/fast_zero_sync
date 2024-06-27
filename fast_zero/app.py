de58344f001abd8b259f486fc89edf377350a028
Isaac Brito <isaac.cris.brito@gmail.com>
2024-06-27 14:06:58 -0300

    Aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+# from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+# @app.get('/quiz', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+# def read_quiz():
+#    return """<html><body><p>Olá mundo</p></body></html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,10 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK        # Assert (Garantir)
     assert response.json() == {'message': 'Olá mundo!'}  # Assert (Garantir)
+
+# def test_read_root_quiz_retornar_ok_html():
+#    client = TestClient(app)
+#    response = client.get('/quiz')
+#
+#    assert response.status_code == HTTPStatus.OK
+#    assert response.text == """<html><body><p>Olá mundo</p></body></html>"""
